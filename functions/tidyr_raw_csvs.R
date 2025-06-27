aac56d118e20a3fc3ef22a2579702a229cf10549
willdrysdale <wsd500@york.ac.uk>
2025-06-27 16:05:31 +0100

    starting scripts to build parquet files

---
+++ functions/tidyr_raw_csvs.R
@@ -0,0 +1,64 @@
+tidy_raw_csvs = function(dataList, type){
+  
+  if(type == "five_hz"){
+    data = dataList |> 
+      dplyr::bind_rows() |> 
+      dplyr::tibble() |> 
+      dplyr::select(
+        unix_time = TheTime,
+        ch1_hz = CH1_Hz,
+        ch2_hz = CH2_Hz,
+        w_ms = w,
+        v_ms = vv,
+        u_ms = u,
+        tempSonic_ms = temp_sonic
+      ) 
+  }
+  
+  if(type == "params"){
+    dataList |> 
+      dplyr::bind_rows() |> 
+      dplyr::tibble() |> 
+      dplyr::select(
+        unix_time = TheTime, 
+        blc_lamp_1 = BLC_Lamp_1, 
+        blc_lamp_2 = BLC_Lamp_2, 
+        blc_temp = MLC_temp,
+        control_temp = Control_Temp,
+        hv_1 = HV_1, 
+        hv_1_cntl = HV_1_cntl, 
+        hv_2 = HV_2,
+        hv_2_cntl = HV_2_cntl, 
+        inlet_pressure = Inlet_pressure,
+        inlet_pressure_set = Inlet_pressure_set,
+        nox_cal = NOx_cal, 
+        no_cal = NO_cal, 
+        no_cal_flow = NO_Cal_flow,
+        no_cal_flow_set = NO_Cal_flow_set,
+        no_valve = NO_valve,
+        oxygen_flow_1 = OxygenFlow_1,
+        oxygen_flow_2 = OxygenFlow_2,
+        oxygen_valve_1 = oxygen_valve_1,
+        oxygen_valve_2 = oxygen_valve_2,
+        ozoniser_1 = ozone_1,
+        ozoniser_2 = ozone_2,
+        pmt_temp = PMT_Temp,
+        reaction_cell_temp = Rxn_Cell_Temp,
+        reaction_cell_pressure = Rxn_Vessel_Pressure,
+        sample_flow_1 = SampleFlow1,
+        sample_flow_set_1 = SampleFlow1_set,
+        sample_flow_set_2 = SampleFlow2_set,
+        vac_valve_1, 
+        vac_valve_2,
+        zero_valve_1,
+        zero_valve_2,
+        zero_volume_temperature,
+        ozone_reset,
+        ozone_trip
+      )
+  }
+  
+  data |> 
+    dplyr::mutate(unixTime = (unixTime-25569)*86400)
+  
+}
---
+++ scripts/build_raw_parquet_files.R
@@ -0,0 +1,80 @@
+# Reads the raw .csv data for 5hz or params, per month and writes a parquet file
+# works on 1 month of data at a time
+# 
+# Designed to be called via an array job to build everything at once
+#
+# uses three trailing arguments corresponding to the type, year and month folders
+# that this job is being pointed at
+
+library(ini)
+library(here)
+library(dplyr)
+library(purrr)
+library(arrow)
+
+source(here::here('functions','tidyr_raw_csvs.R'))
+
+config = read.ini(here::here("config.ini"))
+
+args = commandArgs(trailingOnly = TRUE)
+
+type = args[1] # "five_hz"
+yr = args[2] # "2021"
+mnth = args[3] # "02"
+
+files = list.files(
+  path = file.path(config$paths$raw_data, type, yr, mnth), 
+  full.names = T)
+
+arrowRead = map(files, safely(\(x) arrow::read_csv_arrow(x), NULL))
+
+arrowReadData = arrowRead |> 
+  map(pluck("result"))
+
+arrowReadErrors = arrowRead |> 
+  map(pluck("error"))
+
+whereError = lapply(arrowReadErrors, \(x) !is.null(x)) |> 
+  unlist() |> 
+  which()
+
+# if we got read errors via arrow::read_csv_arrow, try these using read.csv
+if(length(whereError) > 0){ 
+  csvRead = map(files[whereError], safely(\(x) tibble(read.csv(x)), NULL))
+  
+  csvReadData = csvRead |> 
+    map(pluck("result"))
+  
+  csvReadErrors = csvRead |> 
+    map(pluck("error"))
+  
+  for(i in 1:length(whereError)){
+    arrowReadData[[whereError[i]]] = csvReadData[[i]]
+    arrowReadErrors[[whereError[i]]] = csvReadErrors[[i]]
+  }
+  
+}
+
+dataFileName = paste0("NOx_5Hz_", basename(yr),"_", mnth, ".parquet")
+dataDirOut = file.path(config$paths$raw_parquet,"data", type)
+
+if(!dir.exists(dataDirOut)){
+  dir.create(dataDirOut)
+}
+
+write_dataset(data,file.path(dataDirOut, datFileName), format = "parquet")
+
+errors = purrr::discard(arrowReadErrors, \(x) is.null(x))
+
+if(length(errors) > 0){
+  errorFileName = paste0("NOx_5Hz_error_", basename(yr),"_", mnth, ".RDS")
+  
+  errorDirOut = file.path(config$paths$raw_parquet,"error", type)
+  
+  if(!dir.exists(errorDirOut)){
+    dir.create(errorDirOut)
+  }
+  
+  saveRDS(errors, file.path(errorDirOut, errorFileName), format = "parquet")
+
+}
